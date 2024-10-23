bf10f3d99beab2d12284ff03be4a156c27aa404e
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-10-23 14:07:13 +0800

    Add contact number field

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,6 +35,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    contactNumber: {
+      type: String,
+    },
     locationGroup: {
       type: Schema.Types.ObjectId,
       ref: "LocationGroup",
