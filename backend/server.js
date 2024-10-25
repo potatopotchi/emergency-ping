66fa607083acd117600d571e4c938fde079530ad
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-10-25 14:47:55 +0800

    Remove unused code

--- backend/server.js
+++ backend/server.js
@@ -39,51 +39,6 @@ const populateInitialData = async function () {
 
   // Mock data for Location Groups.
   const municipalities = JSON.parse(fs.readFileSync('data/actual_location_Groups.json', 'utf-8'));
-  // await LocationGroup.deleteMany();
-  // const municipalities = [
-  //   {
-  //     region: 'NCR',
-  //     province: 'Metro Manila',
-  //     municipality: 'Quezon City',
-  //     coordinates: [121.0486254, 14.6510546],
-  //   },
-  //   {
-  //     region: 'NCR',
-  //     province: 'Metro Manila',
-  //     municipality: 'City of Manila',
-  //     coordinates: [120.9803621, 14.5904492],
-  //   },
-  //   {
-  //     region: 'NCR',
-  //     province: 'Metro Manila',
-  //     municipality: 'Taguig City',
-  //     coordinates: [121.0744942, 14.5270538],
-  //   },
-  //   {
-  //     region: 'NCR',
-  //     province: 'Metro Manila',
-  //     municipality: 'Pasig City',
-  //     coordinates: [121.0764343, 14.5605166],
-  //   },
-  //   {
-  //     region: 'NCR',
-  //     province: 'Metro Manila',
-  //     municipality: 'Makati City',
-  //     coordinates: [121.0211226, 14.5567949],
-  //   },
-  //   {
-  //     region: 'REG7',
-  //     province: 'Cebu',
-  //     municipality: 'Cebu City',
-  //     coordinates: [123.9019209, 10.2935639],
-  //   },
-  //   {
-  //     region: 'REG3',
-  //     province: 'Tarlac',
-  //     municipality: 'Tarlac City',
-  //     coordinates: [120.5893473, 15.4861218],
-  //   },
-  // ];
 
   for (const item of municipalities) {
     const { region, province, municipality, location } = item;
