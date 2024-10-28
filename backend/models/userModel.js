d12dd8eac568280814e229358ce0cde1734fe98e
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-10-29 01:22:35 +0800

    Change locationGroup type

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -39,7 +39,7 @@ const userSchema = new Schema(
       type: String,
     },
     locationGroup: {
-      type: Schema.Types.ObjectId,
+      type: Schema.Types.String,
       ref: "LocationGroup",
       required: true,
     },
